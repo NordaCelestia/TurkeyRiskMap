2c18032259a93346e8b8078b62e4823279883a0a
NordaCelestia <fatihkocaman797@gmail.com>
2023-11-10 17:59:21 +0300

    Before quit

--- TurkeyRiskMap/Assets/Scripts/buttonHitbox.cs
+++ TurkeyRiskMap/Assets/Scripts/buttonHitbox.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 
 public class buttonHitbox : MonoBehaviour
 {
     public GameObject canvas;
+    public TMP_Text pName, pInfo;
 
     void Start()
     {
@@ -15,6 +17,11 @@ public class buttonHitbox : MonoBehaviour
 
     public void openCanvas()
     {
+        if (this.gameObject.tag == "Ankara")
+        {
+            pName.text = "Ankara";
+            pInfo.text = "daðlýk yer";
+        }
         canvas.SetActive(true);
     }
     
