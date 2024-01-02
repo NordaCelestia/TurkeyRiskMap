aaa7fdf159ced0413de8d1c174e5cedcebe8edf9
ElifCAR <elifcr4@gmail.com>
2024-01-02 13:32:36 +0300

    Haritalar

---
+++ TurkeyRiskMap/Maps/eeartquake_map.py
@@ -0,0 +1,35 @@
+import geopandas as gpd
+import pandas as pd
+import folium
+
+
+gdf_turkiye = gpd.read_file("C:/Users/ipekc/OneDrive/Masaüstü/Proje shp/İl_Sınırı.shp")
+gdf_turkiye = gdf_turkiye.to_crs(epsg=4326)  # Koordinat sistemini düzeltmek için
+
+
+dosya_yolu = 'C:/Users/ipekc/OneDrive/Masaüstü/earthquake.csv'
+data_set = pd.read_csv(dosya_yolu)
+
+data_set = gpd.GeoDataFrame(data_set, geometry=gpd.points_from_xy(data_set['long'], data_set['lat']))
+data_set = data_set.set_crs(epsg=4326)
+
+# Koordinat aralıklarına göre filtreleme
+min_lat, max_lat = 36, 42
+min_lon, max_lon = 26, 45
+gdf_earthquake = data_set.cx[min_lon:max_lon, min_lat:max_lat]
+
+# Folium haritası oluşturme
+m = folium.Map(location=[39, 35], zoom_start=6)
+
+
+for index, row in gdf_earthquake.iterrows():
+    folium.CircleMarker(location=[row['lat'], row['long']],
+                        radius=row['richter'] / 1000,  
+                        color='red',
+                        fill=True,
+                        fill_color='red',
+                        fill_opacity=0.7,
+                        popup=f"Richter: {row['richter']}").add_to(m)
+
+# Haritayı kaydetme
+m.save("earthquake_map.html")
---
+++ TurkeyRiskMap/Maps/map.py
@@ -0,0 +1,43 @@
+import geopandas as gpd
+import folium
+from folium.plugins import MarkerCluster
+import pandas as pd
+
+# Türkiye il sınırları
+gdf_turkiye = gpd.read_file("C:/Users/ipekc/OneDrive/Masaüstü/Proje shp/İl_Sınırı.shp")
+gdf_turkiye = gdf_turkiye.to_crs(epsg=4326)  # Koordinat sistemini düzelt
+
+# Veri setleri
+data_sets = []
+for i in range(2017, 2021):
+    dosya_yolu = 'C:/Users/ipekc/OneDrive/Masaüstü/Yangın/'
+    csv_path = dosya_yolu + f'modis_{i}_Turkey.csv'
+    data_set = pd.read_csv(csv_path)
+    data_sets.append(data_set)
+
+# Veri setinin koordinat sistemini düzeltme
+for i, data_set in enumerate(data_sets):
+    data_sets[i] = gpd.GeoDataFrame(data_set, geometry=gpd.points_from_xy(data_set['longitude'], data_set['latitude']))
+    data_sets[i] = data_sets[i].set_crs(epsg=4326)
+
+# Veri setlerini, sınırlar ile birleştirme
+gdf_fire = gdf_turkiye.copy()
+geometry_column_data_set = 'geometry'
+
+for i, data_set in enumerate(data_sets):
+    gdf_fire = gdf_fire.merge(data_set, how='left', left_on=geometry_column_data_set, right_on=geometry_column_data_set, suffixes=('', f'_{i+1}'))
+
+# Folium haritası oluşturma
+m = folium.Map(location=[39, 35], zoom_start=6)
+
+# Her bir veri setini haritaya ekleme
+for i, data_set in enumerate(data_sets):
+    marker_cluster = MarkerCluster().add_to(m)
+    for index, row in data_set.iterrows():
+        folium.Marker([row['latitude'], row['longitude']], popup=f'Yangın {i+1}').add_to(marker_cluster)
+
+# Türkiye sınırlarını haritaya ekleme
+folium.GeoJson(gdf_turkiye).add_to(m)
+
+m.save('fire_map.html')
+
