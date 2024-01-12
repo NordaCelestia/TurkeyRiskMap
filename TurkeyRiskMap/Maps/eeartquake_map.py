988f4a39bf8d41da086dfe4025c23d4cd7293793
Elif Car <95692232+elifcr@users.noreply.github.com>
2024-01-12 13:09:59 +0300

    Update eeartquake_map.py

--- TurkeyRiskMap/Maps/eeartquake_map.py
+++ TurkeyRiskMap/Maps/eeartquake_map.py
@@ -4,7 +4,7 @@ import folium
 
 
 gdf_turkiye = gpd.read_file("C:/Users/ipekc/OneDrive/Masaüstü/Proje shp/İl_Sınırı.shp")
-gdf_turkiye = gdf_turkiye.to_crs(epsg=4326)  # Koordinat sistemini düzeltmek için
+gdf_turkiye = gdf_turkiye.to_crs(epsg=4326) 
 
 
 dosya_yolu = 'C:/Users/ipekc/OneDrive/Masaüstü/earthquake.csv'
@@ -13,12 +13,10 @@ data_set = pd.read_csv(dosya_yolu)
 data_set = gpd.GeoDataFrame(data_set, geometry=gpd.points_from_xy(data_set['long'], data_set['lat']))
 data_set = data_set.set_crs(epsg=4326)
 
-# Koordinat aralıklarına göre filtreleme
 min_lat, max_lat = 36, 42
 min_lon, max_lon = 26, 45
 gdf_earthquake = data_set.cx[min_lon:max_lon, min_lat:max_lat]
 
-# Folium haritası oluşturme
 m = folium.Map(location=[39, 35], zoom_start=6)
 
 
@@ -31,5 +29,4 @@ for index, row in gdf_earthquake.iterrows():
                         fill_opacity=0.7,
                         popup=f"Richter: {row['richter']}").add_to(m)
 
-# Haritayı kaydetme
 m.save("earthquake_map.html")
